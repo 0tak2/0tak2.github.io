9ca4f227f2606cd06d39025a1e9d8244efb6c060
0tak2 <0tak2.code@gmail.com>
2024-01-07 16:50:59 +0900

    feat: 메인 페이지 배지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,21 @@
 		상상을 컴파일하는 <br />
 		백엔드 엔지니어
 	</h2>
+
+	<span class="badges">
+		<a href="https://github.com/0tak2">
+			<img src="https://img.shields.io/badge/-github-7C7877?style=flat&logo=GitHub&link=https://github.com/0tak2">
+		</a>
+		<a href="https://archiveyoung.tistory.com">
+			<img src="https://img.shields.io/badge/-blog-7C7877?style=flat&logo=Tistory&link=https://archiveyoung.tistory.com">
+		</a>
+		<a href="https://instagram.com/0tag2_98">
+			<img src="https://img.shields.io/badge/-instagram-7C7877?style=flat&logo=Instagram&link=https://instagram.com/0tag2_98">
+		</a>
+		<a href="https://0tak2.github.io/T0L">
+			<img src="https://img.shields.io/badge/-Today%20I%20Learned-7C7877?style=flat&logo=gitbook&link=https://0tak2.github.io/T0L">
+		</a>
+	</span>
 </section>
 
 <Monaco />
@@ -34,5 +49,9 @@
 	h1 {
 		width: 100%;
 		font-weight: 600;
+	}
+
+	.badges {
+		margin-top: 1.5rem;
 	}
 </style>
