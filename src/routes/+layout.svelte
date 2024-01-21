5a69168bc6bc6bb9204743eb7c4617034f34defc
0tak2 <0tak2.code@gmail.com>
2024-01-21 16:39:40 +0900

    change: 카운터 제거

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,6 @@
 	<footer>
 		<p>
 			<a href="mailto:0tak2.code@gmail.com">임영택</a>, 2024<br />
-			<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2F0tak2.github.io&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
 		</p>
 	</footer>
 </div>
